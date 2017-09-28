a7648fbb08dfcdbe85e5854b4e4fc1bfbf9f7104
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-09-28 16:35:47 -0400

    added schema.js and seeds.js

---
+++ db/schema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+// First, we instantiate a namespace for our Schema constructor defined by mongoose.
+const Schema = mongoose.Schema;
+
+const SongSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+})
+
+const PlaylistSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    songs: [SongSchema]
+});
+
+
+
+const PlaylistModel = mongoose.model('Playlist', PlaylistSchema)
+const SongModel = mongoose.model('Song', SongSchema)
+
+module.exports = {
+    CompanyModel: CompanyModel,
+    SnowboardModel: SnowboardModel
+}
---
+++ db/seeds.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+var mongoose = require('mongoose');
+var Schema = require("./schema.js");
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection;
+
+// Will log an error if db can't connect to MongoDB
+db.on('error', function (err) {
+    console.log(err);
+});
+
+// Will log "database has been connected" if it successfully connects.
+db.once('open', function () {
+    console.log("database has been connected!");
+});
+
+var PlaylistModel = Schema.PlaylistModel;
+var SongModel = Schema.SongModel;
+
+PlaylistModel.remove({}, function (err) {
+    console.log(err);
+});
+
+const burton = new PlaylistModel({ name: 'Burton', country: 'US'})
+const dc = new PlaylistModel({ name: 'DC', country: 'US'})
+const ktwo = new PlaylistModel({ name: 'K2', country: 'Canada'})
+
+const littleSong = new SongModel({ name: 'Little Song', price: 123.45}) 
+const bigSong = new SongModel({ name: 'Big Song', price: 123.45}) 
+const blueSong = new SongModel({ name: 'Blue Song', price: 123.45}) 
+
+const playlits = [burton, dc, ktwo]
+const songs = [littleSong, bigSong, blueSong]
+
+
+// Here we assign some projects to each student.
+playlits.forEach((playlist) => {
+
+    playlist.songs = songs
+
+    playlist.save()
+        .then((playlist) => {
+            console.log(`${playlist.name} saved!`)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+});
+
+// Disconnect from database
+db.close();
