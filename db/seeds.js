20c2d62127ebf0384bb40fae978bc97065dd531f
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-09-29 15:33:03 -0400

    added background

--- db/seeds.js
+++ db/seeds.js
@@ -28,12 +28,12 @@ const soulSauce = new PlaylistModel({ name: 'SoulSauce', country: 'US'})
 const trapSauce = new PlaylistModel({ name: 'TrapSauce', country: 'US'})
 const hotSauce = new PlaylistModel({ name: 'HotSauce', country: 'Canada'})
 
-const littleSong = new SongModel({ name: 'Little Song', price: 123.45}) 
-const bigSong = new SongModel({ name: 'Big Song', price: 123.45}) 
-const blueSong = new SongModel({ name: 'Blue Song', price: 123.45}) 
+const controlla = new SongModel({ name: 'Controlla', price: 123.45}) 
+const oneDance = new SongModel({ name: 'One Dance', price: 123.45}) 
+const uWithMe = new SongModel({ name: 'U With Me?', price: 123.45}) 
 
 const playlists = [soulSauce, trapSauce, hotSauce]
-const songs = [littleSong, bigSong, blueSong]
+const songs = [controlla, oneDance, uWithMe]
 
 
 // Here we assign some projects to each student.
