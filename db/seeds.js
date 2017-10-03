28892e62291951c7fa3de8138b0ace433e9c0cd0
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-10-03 13:23:30 -0400

    completed database

--- db/seeds.js
+++ db/seeds.js
@@ -35,24 +35,29 @@ const hotSauce = new PlaylistModel({ name: 'Hot', creator: 'Sauce Radio'})
 
 const controlla = new SongModel({ name: 'Controlla', artist: 'Drake', mp3:'http://wethesauce.com/wp-content/uploads/2016/06/11.-Controlla.mp3'}) 
 const sleepWalkin = new SongModel({ name: 'Sleep Walkin', artist: 'Mozzy', mp3:'http://wethesauce.com/wp-content/uploads/2017/08/05.-Sleep-Walkin.mp3'}) 
-const uWithMe = new SongModel({ name: 'U With Me?', artist: 'Drake', mp3:'http://wethesauce.com/wp-content/uploads/2016/06/11.-Controlla.mp3'}) 
+const uWithMe = new SongModel({ name: 'U With Me?', artist: 'Drake', mp3:'http://wethesauce.com/wp-content/uploads/2016/06/03.-U-With-Me.mp3'}) 
 const getYou = new SongModel({ name: 'Get You', artist: 'Daniel Caesar (feat. Kali Uchis)', mp3:'http://wethesauce.com/wp-content/uploads/2017/08/01-Get-You-feat.-Kali-Uchis.mp3'}) 
-const oneDance = new SongModel({ name: 'One Dance2', artist: 'Drake', mp3:'http://wethesauce.com/wp-content/uploads/2016/06/11.-Controlla.mp3'}) 
-const uWithMe2 = new SongModel({ name: 'U With Me?2', artist: 'Drake', mp3:'http://wethesauce.com/wp-content/uploads/2016/06/11.-Controlla.mp3'}) 
+const oneDance = new SongModel({ name: 'One Dance', artist: 'Drake(Ft. Wizkid & Kyla)', mp3:'http://wethesauce.com/wp-content/uploads/2016/06/12.-One-Dance-Ft.-Wizkid-Kyla.mp3'}) 
+const iDoLoveYou = new SongModel({ name: 'I do love you', artist: 'GQ', mp3:'http://wethesauce.com/wp-content/uploads/2017/07/gq_i_do_love_you-lookingforlyrics.org_.mp3'}) 
+const bankAccount = new SongModel({ name: 'Bank Account', artist: '21 Savage', mp3:'http://wethesauce.com/wp-content/uploads/2017/07/02.-Bank-Account.mp3'}) 
+const wildThoughts = new SongModel({ name: 'Wild thoughts', artist: 'DJ Khaled(feat. Rihanna & Bryson Tiller)', mp3:'http://wethesauce.com/wp-content/uploads/2017/06/04-Wild-Thoughts-feat.-Rihanna-Br.m4a'}) 
+const firstDayOut = new SongModel({ name: 'First Day Out', artist: 'Tee Grizzley', mp3:'http://wethesauce.com/wp-content/uploads/2017/04/02.-First-Day-Out.mp3'}) 
 
 const andre = new UserModel({name: 'Andre', email: 'Andre@sauceradio.com'})
 
 const playlists = [soulSauce, trapSauce, hotSauce]
-const tsongs = [sleepWalkin, oneDance, uWithMe]
-const song2 = [controlla, getYou, oneDance]
+const trap = [sleepWalkin, bankAccount, firstDayOut]
+const soul = [controlla, getYou, iDoLoveYou]
+const hot = [oneDance, uWithMe,wildThoughts]
 const users = [andre]
 
 
 // Here we assign some projects to each student.
 playlists.forEach((playlist) => {
 
-    playlist.songs = tsongs
-    soulSauce.songs = song2
+    trapSauce.songs = trap
+    soulSauce.songs = soul
+    hotSauce.songs = hot
 
     playlist.save()
         .then((playlist) => {
